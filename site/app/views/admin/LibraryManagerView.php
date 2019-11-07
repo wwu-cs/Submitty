aad88afae0cb34c71bf6f99657e1a30a85419354
CalebJ2 <calebinvents@gmail.com>
2019-11-07 09:48:29 -0800

    Add library manager zip upload form

--- site/app/views/admin/LibraryManagerView.php
+++ site/app/views/admin/LibraryManagerView.php
@@ -2,10 +2,13 @@
 
 namespace app\views\admin;
 
+use app\libraries\FileUtils;
 use app\views\AbstractView;
 
 class LibraryManagerView extends AbstractView {
     public function showLibraryManager(String $text) {
+        $this->core->getOutput()->addInternalJs("drag-and-drop.js");
+        $this->core->getOutput()->addInternalCss(FileUtils::joinPaths('fileinput.css'));
         return $this->core->getOutput()->renderTwigTemplate("admin/LibraryManager.twig", [
             "text" => $text
         ]);
