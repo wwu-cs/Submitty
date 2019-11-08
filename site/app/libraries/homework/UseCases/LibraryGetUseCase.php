73f1e83238ab28a1fddc8fd90ae813432e69c478
dakriy <git@persignum.com>
2019-11-08 09:39:14 -0800

    Add use case for getting all libraries

---
+++ site/app/libraries/homework/UseCases/LibraryGetUseCase.php
@@ -0,0 +1,52 @@
+<?php namespace app\libraries\homework\UseCases;
+
+
+use app\libraries\Core;
+use app\libraries\homework\Gateways\Library\LibraryGatewayFactory;
+use app\libraries\homework\Gateways\LibraryGateway;
+
+class LibraryGetResponse {
+    public $error;
+
+    /** @var string[] */
+    protected $libraries = [];
+
+    public function addLibrary(string $lib) {
+        $this->libraries[] = $lib;
+    }
+
+    public function getLibraries(): array {
+        return $this->libraries;
+    }
+
+    public static function error(string $message) {
+        $response = new static;
+        $response->error = $message;
+        return $response;
+    }
+}
+
+
+class LibraryGetUseCase extends BaseUseCase {
+
+    /** @var LibraryGateway */
+    protected $gateway;
+
+    public function __construct(Core $core) {
+        parent::__construct($core);
+
+        $this->gateway = LibraryGatewayFactory::getInstance();
+    }
+
+    public function getLibraries(): LibraryGetResponse {
+        $response = new LibraryGetResponse();
+
+        $libraries = $this->gateway->getAllLibraries($this->location);
+
+        foreach ($libraries as $library) {
+            $response->addLibrary($library);
+        }
+
+        return $response;
+    }
+}
