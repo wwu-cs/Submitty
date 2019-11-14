6ef8a97dc12d13301ee20fa0e1090a6861d97080
CalebJ2 <calebinvents@gmail.com>
2019-11-13 23:46:02 -0800

    Change library manager forms to use ajax and pass the csrf_token

--- site/app/views/admin/LibraryManagerView.php
+++ site/app/views/admin/LibraryManagerView.php
@@ -14,9 +14,10 @@ class LibraryManagerView extends AbstractView {
         return $this->core->getOutput()->renderTwigTemplate("admin/library/LibraryManager.twig", [
             'text' => $text,
             'libraries' => $libraries,
-            'git_submit_url' => $this->core->buildUrl(['homework/library/manage', 'upload', 'git']),
-            'filepath_submit_url' => $this->core->buildUrl(['homework/library/manage', 'upload', 'filepath']),
-            'zip_submit_url' => $this->core->buildUrl(['homework/library/manage', 'upload', 'zip'])
+            "zip_submit_url" => $this->core->buildUrl(['homework/library', 'manage', 'upload', 'zip']),
+            "git_submit_url" => $this->core->buildUrl(['homework/library', 'manage', 'upload', 'git']),
+            "filepath_submit_url" => $this->core->buildUrl(['homework/library', 'manage', 'upload', 'filepath']),
+            "csrf_token" => $this->core->getCsrfToken()
         ]);
     }
 }
